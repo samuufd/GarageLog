11631e076e018bc51bb679628ec3a6ced3462d07
samuufd <samuufd@github.com>
2026-06-10 22:07:02 +0200

    Remove default smoke test

--- garage_log/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:garage_log/providers/providers.dart';
-
-void main() {
-  testWidgets('App smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const Providers());
-    expect(find.text('GarageLog'), findsOneWidget);
-  });
-}
